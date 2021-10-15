d922be0eae8d4799390e3397cd3bece38ee72d05
alexmuniz96 <alexmuniz096@gmail.com>
2021-10-15 20:23:23 -0300

    slide basic functionality

--- js/slide.js
+++ js/slide.js
@@ -5,28 +5,30 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
+  moveSlide(distX) {
+    this.slide.style.transform = `translate3d(${distX}px,0,0)`
+    this.dist.movePosition = distX
+  }
+
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.5
+    return this.dist.finalPosition - this.dist.movement
+  }
+
   onStart(event) {
     event.preventDefault()
-    this.wrapper.addEventListener("mousemove", this.onMove)
     this.dist.startX = event.clientX
+    this.wrapper.addEventListener("mousemove", this.onMove)
   }
 
   onMove(event) {
-    const finalPosition = this.dist.startX - event.clientX
+    const finalPosition = this.updatePosition(event.clientX)
     this.moveSlide(finalPosition)
-  }
-
-  moveSlide(distX) {
-    this.slide.style.transform = `tranlate3d${distX},0,0`
-  }
-
-  updatePosition(event) {
-    this.dist.movement = this.dist.startX - event.clientX
-    return movement
   }
 
   onEnd(event) {
     this.wrapper.removeEventListener("mousemove", this.onMove)
+    this.dist.finalPosition = this.dist.movePosition
   }
 
   addSlideEvents() {
